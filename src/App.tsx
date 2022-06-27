bbb753aaf391e92e8d9b010f615a0912d23b998f
lucaspesso4 <lucas.pessoa@mobills.com.br>
2022-06-27 17:26:03 -0300

    chore: add native-base dependency, custom paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-import SVGImage from './assets/icons/shopping-cart.svg';
+import {NativeBaseProvider} from 'native-base';
+import {StatusBar, useColorScheme} from 'react-native';
 
 export function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <NativeBaseProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View
-        style={{
-          backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        }}>
-        <SVGImage width={100} height={100} fill="#000" />
-        <Text>TON PRODUCT SALE</Text>
-      </View>
-    </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
